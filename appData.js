fb881ece3ba5adc3e0a5d21aae1315e1e0b74305
Wojciech Szczyrba <benice195@gmail.com>
2017-02-21 13:25:20 +0100

    ux visual fix and upgrade

--- appData.js
+++ appData.js
@@ -3,6 +3,11 @@ var url ="http://api.openweathermap.org/data/2.5/weather?q=";
 
 var miasto = $("#town");
 $("#search").click(szukajMiasta);
+$("#nazwa-miasta").keypress( function(e) {
+        if(e.which === 13) {
+                szukajMiasta();
+        };
+});
 
 function szukajMiasta() {
     var nazwaMiasta = $("#nazwa-miasta").val();
@@ -49,41 +54,59 @@ function pokazMiasto(resp) {
 
 
     if (mias.weather[0].icon === "01d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/01d.png'</img>");
+            $("#pic").append("<img src='png/sun.png'></img>");
+            $("body").css('background-image', 'url("background/pogodnydzien.jpg")');
     } else if (mias.weather[0].icon === "01n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/01n.png'</img>");
+            $("#pic").append("<img src='png/moon.png'></img>");
+            $("body").css('background-image', 'url("background/pogodnanoc.jpg")');
     } else if (mias.weather[0].icon === "02d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/02d.png'</img>");
+            $("#pic").append("<img src='png/suncloud.png'></img>");
+            $("body").css('background-image', 'url("background/lekkozachmurzonydzien.jpg")');
     } else if (mias.weather[0].icon === "02n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/02n.png'</img>");
+            $("#pic").append("<img src='png/mooncloud.png'></img>");
+            $("body").css('background-image', 'url("background/lekkozachmurzonanoc1.jpg")');
     } else if (mias.weather[0].icon === "03d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/03d.png'</img>");
+            $("#pic").append("<img src='png/cloud.png'></img>");
+            $("body").css('background-image', 'url("background/lekkozachmurzonydzien.jpg")');
     } else if (mias.weather[0].icon === "03n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/03n.png'</img>");
+            $("#pic").append("<img src='png/cloud.png'></img>");
+            $("body").css('background-image', 'url("background/zachmurzonanoc.jpg")');
     } else if (mias.weather[0].icon === "04d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/04d.png'</img>");
+            $("#pic").append("<img src='png/cloudy.png'></img>");
+            $("body").css('background-image', 'url("background/zachmurzonydzien.jpg")');
     } else if (mias.weather[0].icon === "04n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/04n.png'</img>");
+            $("#pic").append("<img src='png/cloudy.png'></img>");
+            $("body").css('background-image', 'url("background/zachmurzonanoc.jpg")');
     } else if (mias.weather[0].icon === "09d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/09d.png'</img>");
+            $("#pic").append("<img src='png/rain.png'></img>");
+            $("body").css('background-image', 'url("background/dziendeszcz.jpg")');
     } else if (mias.weather[0].icon === "09n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/09n.png'</img>");
+            $("#pic").append("<img src='png/rain.png'></img>");
+            $("body").css('background-image', 'url("background/nocdeszcz.jpg")');
     } else if (mias.weather[0].icon === "10d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/10d.png'</img>");
+            $("#pic").append("<img src='png/sunrain.png'></img>");
+            $("body").css('background-image', 'url("background/dziendeszcz.jpg")');
     } else if (mias.weather[0].icon === "10n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/10n.png'</img>");
+            $("#pic").append("<img src='png/moonrain.png'></img>");
+            $("body").css('background-image', 'url("background/nocdeszcz.jpg")');
     } else if (mias.weather[0].icon === "11d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/11d.png'</img>");
+            $("#pic").append("<img src='png/dark.png'></img>");
+            $("body").css('background-image', 'url("background/dzienburza.jpg")');
     } else if (mias.weather[0].icon === "11n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/11n.png'</img>");
+            $("#pic").append("<img src='png/dark.png'></img>");
+            $("body").css('background-image', 'url("background/nocburza.jpg")');
     } else if (mias.weather[0].icon === "13d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/13d.png'</img>");
+            $("#pic").append("<img src='png/snow.png'></img>");
+            $("body").css('background-image', 'url("background/dziensnieg.jpg")');
     } else if (mias.weather[0].icon === "13n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/13n.png'</img>");
+            $("#pic").append("<img src='png/snow.png'></img>");
+            $("body").css('background-image', 'url("background/nocsnieg.jpg")');
     } else if (mias.weather[0].icon === "50d") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/50d.png'</img>");
+            $("#pic").append("<img src='png/mist.png'></img>");
+            $("body").css('background-image', 'url("background/dzienmgla.jpg")');
     } else if (mias.weather[0].icon === "50n") {
-            $("#pic").append("<img src='http://openweathermap.org/img/w/50n.png'</img>");
+            $("#pic").append("<img src='png/mist.png'></img>");
+            $("body").css('background-image', 'url("background/nocmgla.jpg")');
     };
 
     data();
